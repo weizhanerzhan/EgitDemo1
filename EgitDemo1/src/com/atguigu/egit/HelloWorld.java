2936b01bc4081abfbe62c81953722572e90dd162
weizhanerzhan1 <xiao851792570@126.com>
2017-05-22 21:24:39 +0800

    two

--- EgitDemo1/src/com/atguigu/egit/HelloWorld.java
+++ EgitDemo1/src/com/atguigu/egit/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("为站而战！");
+		System.out.println("为站而战！");
+		System.out.println("为站而战！");
+		System.out.println("为站而战！");
 	}
 }
